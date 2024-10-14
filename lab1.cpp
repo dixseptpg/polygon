53bafafa10b38991e7dd6d4eb2b7db29c7e37b70
GorshkovaSofía <gray.oriole@gmail.com>
2024-10-14 15:33:28 +0300

    лаба 1,,,

--- lab1.cpp
+++ lab1.cpp
@@ -1,45 +1,5 @@
-// #include <iostream>
-// #include <vector>
-// #include <string>
-
-// using namespace std;
-// //Найти количество нечётных чисел в массиве
-
-// //def Get_len_odd;
-// /*a variable -- 
-// Type name;
-// Type name1, name2, name3;
-
-// */
-
-// int main()
-// {
-//     int n;
-//     int a[n];
-//     a = input("Введите массив: "); //thats an array
-//     for (int i = 0; i < n; ++i)
-//     {
-//         a[i] = i + 1;
-//     }
-
-//     for i in a
-//         if n % 2 == 0
-
-
-//     std::vector 
-//     std::array
-//     int number;
-
-//     a = input(array)
-
-
-//     return 0;
-// }
-
-
-
-
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -50,13 +10,12 @@ int main() {
 
     int arr[n]; //Массив заданного размера
 
-    // Вводим элементы массива
-    cout << "Элементы массива: ";
+    cout << "Элементы массива: "; // Вводим элементы массива
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    int count = 0; // Счётчик нечётных чисел
+    int odd_count = 0; // Счётчик нечётных чисел
 
     // Проходим по массиву и считаем нечётные числа
     for (int i = 0; i < n; i++) {
